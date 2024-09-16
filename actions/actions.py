522db51498df402f9ce098840b0bbfd1498de06b
Blix Quirante <92987751+BlexBOTTT@users.noreply.github.com>
2024-09-16 17:06:14 +0800

    added resp. for unidentified intent, school-loc intent

--- actions/actions.py
+++ actions/actions.py
@@ -9,8 +9,11 @@
 
 # from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+#from rasa_sdk.executor import CollectingDispatcher
+
+# Fallback Policy
+from rasa_sdk.events import UserUtteranceReverted
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +28,11 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionDefaultFallback(Action):
+    def name(self):
+        return "action_default_fallback"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="Sorry, I didn't understand that. Can you rephrase?")
+        return [UserUtteranceReverted()]
