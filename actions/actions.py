060b56e3468bbc624c4026f1676db79f65ebcf65
BlexBOTTT <blexbottt2915@gmail.com>
2024-11-20 11:28:14 +0800

    completion of code, 200 epochs, 70-30 and CV testing results, NO BERT INTEGRATION)

--- actions/actions.py
+++ actions/actions.py
@@ -66,20 +66,21 @@ class ActionFetchDynamicResponse(Action):
             return []
 
         # Get the latest intent from the user's message
-        intent = tracker.latest_message['intent']['name']
+        rasa_intent = tracker.latest_message['intent']['name']
 
         # debugger print on which intent is selected
-        print("Action: AFDR Detected intent:", intent)
+        print("Action: AFDR Detected intent:", rasa_intent)
 
         # Loop through all collections in the database
         for collection_name in self.db.list_collection_names():
             # Fetch the course name from the tracker slot
             collection = self.db[collection_name]
 
-            response = "I'm sorry, I couldn't find the information you're looking for."  # Default response
+            # Default response if no response fetched from monbodb due to undetected intent
+            response = "I'm sorry, I couldn't find the information you're looking for."  
 
             # Special handling for the 'courses' collection when asking about tuition
-            if collection_name == "tuition_prices" and intent == "ask_tuition_price_specific":
+            if collection_name == "tuition_prices" and rasa_intent == "ask_tuition_price_specific":
                 course_name = tracker.get_slot('course')
                 print("Detected course:", course_name)
 
@@ -90,11 +91,11 @@ class ActionFetchDynamicResponse(Action):
                     }
                 else:
                     # Fallback to general tuition query if no course name is available
-                    query = build_query(intent, collection_name)
+                    query = build_query(rasa_intent, collection_name)
 
             else: 
                 # Use the helper function to build the query, fallback to a broader query
-                query = build_query(intent, collection_name)
+                query = build_query(rasa_intent, collection_name)
 
             try:
             # Execute the query on the current collection
@@ -102,17 +103,30 @@ class ActionFetchDynamicResponse(Action):
 
                 # Check if a result was found
                 if result:
-
                      # Format response based on collection type
+                        # then match detected rasa intent
+                            # then spits the according utterance to the user
                     if collection_name == "admission":    
-                        response = f"{result.get('utter_admission', 'No details available.')}"
+                        if rasa_intent == "ask_admission_process":
+                            response = result.get('utter_admission_process', 'Admission process not details  available.')
+                        if rasa_intent == "ask_admission_requirements":
+                            response = result.get('utter_admission_requirements', 'Admission requirements details not available.')
+                        if rasa_intent == "ask_admission_online_registration":
+                            response = result.get('utter_admission_online_registration', 'Admission online registration details  not available.')
+                        if rasa_intent == "ask_admission_transferee_enrollment":
+                            response = result.get('utter_admission_transferee_enrollment', 'Admission transferee enrollment details  not available.')
+                        if rasa_intent == "ask_admission_working_student":
+                            response = result.get('utter_admission_working_student', 'Admission working student details not available.')
 
                     elif collection_name == "courses":
-                        response = f"{result.get('utter_course_list', 'No details available.')}"
+                        if rasa_intent == "ask_courses_list":
+                            response = result.get('utter_course_list', 'Course list details not available.')
                     
                     elif collection_name == "discounts":    
-                        response = f"{result.get('utter_discounts', 'No details available.')}"
-
+                        if rasa_intent == "ask_available_discounts":
+                            response = result.get('utter_discounts', 'Discount details not available.')
+                    
+                    # only to show the feedback yes/no response counter:
                     elif collection_name == "feedback":    
                         yes_count = result.get('yes_count', 0)  # Default to 0 if not found
                         no_count = result.get('no_count', 0)   # Default to 0 if not found
@@ -120,24 +134,24 @@ class ActionFetchDynamicResponse(Action):
                         response = f"Correct Response: {yes_count}\nWrong Response: {no_count}"
 
                     elif collection_name == "general":
-                        if intent == "ask_school_location":
-                            response = result.get('utter_school_location', 'Location details not available.')
-                        elif intent == "ask_school_contact":
-                            response = result.get('utter_ask_contact', 'Contact details not available.')
-                        elif intent == "ask_chatbot_coverage":
-                            response = result.get('utter_chatbot_coverage', 'Contact details not available.')
-                        elif intent == "ask_speak_to_staff":
-                            response = result.get('utter_speak_to_staff', 'Contact details not available.')
-                        elif intent == "ask_why_should_i_enroll":
-                            response = result.get('utter_why_should_i_enroll', 'Contact details not available.')
+                        if rasa_intent == "ask_school_location":
+                            response = result.get('utter_school_location', 'School location not available.')
+                        elif rasa_intent == "ask_school_contact":
+                            response = result.get('utter_ask_contact', 'School contact details not available.')
+                        elif rasa_intent == "ask_chatbot_coverage":
+                            response = result.get('utter_chatbot_coverage', 'Chatbot coverage details not available.')
+                        elif rasa_intent == "ask_speak_to_staff":
+                            response = result.get('utter_speak_to_staff', 'Speak-to-staff details not available.')
+                        elif rasa_intent == "ask_why_should_i_enroll":
+                            response = result.get('utter_why_should_i_enroll', 'Why-enroll details not available.')
                         else:
                             response = result.get('No general details available.')  
                 
                     elif collection_name == "tuition_prices":
-                        response = result.get('utter_tuition_price_specific', 'No tuition available.')
-                        if intent == "ask_tuition_price_general":
-                            response = result.get('utter_tuition_price_general', 'No tuition available.')
-
+                        if rasa_intent == "ask_tuition_price_specific":
+                            response = result.get('utter_tuition_price_specific', 'Specific course tuition price not available.')
+                        if rasa_intent == "ask_tuition_price_general":
+                            response = result.get('utter_tuition_price_general', 'General tuition price not available.')
 
                     # Send the response back to the user
                     dispatcher.utter_message(text=response)
@@ -148,57 +162,57 @@ class ActionFetchDynamicResponse(Action):
                 continue
 
         # If no information was found in any collection in the DB, send an apology message
-        dispatcher.utter_message(text="LAST I'm sorry, I couldn't find the information you're looking for.")
+        dispatcher.utter_message(text=response)
         return []
 ###
 
 ############################################################################################################################
 ############################################################################################################################
 
-class ActionIterateHelpfulCounter(Action):
-    def name(self) -> str:
-        return "action_iterate_helpful_counter"
+# class ActionIterateHelpfulCounter(Action):
+#     def name(self) -> str:
+#         return "action_iterate_helpful_counter"
 
-    def __init__(self):
-        # MongoDB connection setup
-        self.client = MongoClient("mongodb://localhost:27017/")
-        self.db = self.client["admission_chatbot"]
-        self.collection = self.db["feedback"]
+#     def __init__(self):
+#         # MongoDB connection setup
+#         self.client = MongoClient("mongodb://localhost:27017/")
+#         self.db = self.client["admission_chatbot"]
+#         self.collection = self.db["feedback"]
 
-        # Triggers if there is no helpful_counter ID in the collection feedback
-        if not self.collection.find_one({"_id": "helpful_counter"}):
-            self.collection.insert_one({"_id": "helpful_counter", "yes_count": 0, "no_count": 0})
+#         # Triggers if there is no helpful_counter ID in the collection feedback
+#         if not self.collection.find_one({"_id": "helpful_counter"}):
+#             self.collection.insert_one({"_id": "helpful_counter", "yes_count": 0, "no_count": 0})
 
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list[EventType]:
-        # Get the user's intent from the latest message
-        user_intent = tracker.latest_message['intent']['name']
+#     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list[EventType]:
+#         # Get the user's intent from the latest message
+#         user_intent = tracker.latest_message['intent']['name']
 
-        print("Action: AIHC Detected intent:", user_intent) # debugging line
+#         print("Action: AIHC Detected intent:", user_intent) # debugging line
 
-        try:
-            # Update counter based on user intent
-            if user_intent == "affirm":
-                result = self.collection.update_one({"_id": "helpful_counter"}, {"$inc": {"yes_count": 1}})
-                # print(f"Update result: {result.modified_count}")  # Debugging line
-                dispatcher.utter_message(text="Thank you for your feedback! Please do continue asking inquiries!")
+#         try:
+#             # Update counter based on user intent
+#             if user_intent == "affirm":
+#                 result = self.collection.update_one({"_id": "helpful_counter"}, {"$inc": {"yes_count": 1}})
+#                 # print(f"Update result: {result.modified_count}")  # Debugging line
+#                 dispatcher.utter_message(text="Thank you for your feedback! Please do continue asking inquiries!")
 
-            elif user_intent == "deny":
-                result = self.collection.update_one({"_id": "helpful_counter"}, {"$inc": {"no_count": 1}})
-                # print(f"Update result: {result.modified_count}")  # Debugging line
-                dispatcher.utter_message(text="Thank you for letting us know! We'll keep improving.")
+#             elif user_intent == "deny":
+#                 result = self.collection.update_one({"_id": "helpful_counter"}, {"$inc": {"no_count": 1}})
+#                 # print(f"Update result: {result.modified_count}")  # Debugging line
+#                 dispatcher.utter_message(text="Thank you for letting us know! We'll keep improving.")
 
-            else:
-                response = 'No tuition details available.'
+#             else:
+#                 response = 'no affirm-deny response detected.'
 
-            # # DEBUG: Fetch the updated document to confirm the count
-            # feedback_counter = self.collection.find_one({"_id": "helpful_counter"})
-            # print(f"Updated document: {feedback_counter}")  # Debugging line
+#             # # DEBUG: Fetch the updated document to confirm the count
+#             # feedback_counter = self.collection.find_one({"_id": "helpful_counter"})
+#             # print(f"Updated document: {feedback_counter}")  # Debugging line
 
-        except Exception as e:
-            dispatcher.utter_message(text=response)
-            print(f"Error updating feedback counter: {e}")
+#         except Exception as e:
+#             dispatcher.utter_message(text=response)
+#             print(f"Error updating feedback counter: {e}")
 
-        return []
+#         return []
 
 ############################################################################################################################
 ############################################################################################################################
